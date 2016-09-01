336c8ec48de2668f606e96eebbe9f8534316da69
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2016-09-01 07:06:48 +0200

    there was an error when calling pipe(). Interface to listen now as command line parameter

--- 6to4proxy.js
+++ 6to4proxy.js
@@ -1,15 +1,15 @@
 var net = require('net');
 
-var portToForward = 8881;
+var portToForward = 18888;
 
 var server6 = net.createServer(function(sock6) { //'connection' listener
-	console.log('sock6 connected');
+	console.log('connection from: ' + sock6.remoteAddress);
 	
 	var conn4 = net.connect({port: portToForward}, function() { 
-		console.log('connected to ipv4 port');
+		//console.log('connected to ipv4 port');
 		
 		conn4.pipe(sock6);
-		server6.pipe(conn4);
+		sock6.pipe(conn4);
 		
 	}); 
 	
@@ -18,7 +18,10 @@ var server6 = net.createServer(function(sock6) { //'connection' listener
 	});
 });
 
-server6.listen(portToForward, '::', function() { //'listening' listener
-  console.log('server6 is listening');
+//var ip6toListen = '::0'; // should be all v6 interfaces
+var ip6toListen = process.argv[2];
+
+server6.listen(portToForward, ip6toListen, function() { //'listening' listener
+  console.log('6to4 server is listening on nterface ' + ip6toListen + " port: " + portToForward);
   console.dir( server6.address() );
 });
